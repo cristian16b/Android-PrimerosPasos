c23bfdcdc2cf101623635d416add49536fd938b1
cristian16b <cristian.budzicz@gmail.com>
2019-06-11 20:34:07 -0300

    agregada funcionalidad sumar

--- app/src/main/java/com/example/usuario/myapplication/MainActivity.java
+++ app/src/main/java/com/example/usuario/myapplication/MainActivity.java
@@ -3,18 +3,35 @@ package com.example.usuario.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText numeroI;
+    private EditText numeroII;
+    private TextView resultadoSuma;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //
+        //instancio los elementos
+        numeroI = (EditText)findViewById(R.id.primerNumero);
+        numeroII = (EditText)findViewById(R.id.segundoNumero);
+        resultadoSuma = (TextView)findViewById(R.id.resultadoSuma);
     }
 
     public void sumar(View view) {
-        
+
+        String primerNumero = numeroI.getText().toString();
+        String segundoNumero = numeroII.getText().toString();
+        int nro1 = Integer.parseInt(primerNumero);
+        int nro2 = Integer.parseInt(segundoNumero);
+        int suma = nro1 + nro2;
+        String Suma =  Integer.toString(suma);
+
+        resultadoSuma.setText("El resultado es " +  Suma);
     }
 }
